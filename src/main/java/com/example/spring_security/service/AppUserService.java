a525a01a67479fbb83bcae7c1db9837678f55233
d1master1 <barancev2006@gmail.com>
2025-03-05 11:17:15 +0300

    fix_error

--- src/main/java/com/example/spring_security/service/AppUserService.java
+++ src/main/java/com/example/spring_security/service/AppUserService.java
@@ -1,33 +1,54 @@
 package com.example.spring_security.service;
 
-import com.example.spring_security.model.UserDto;
+
+import com.example.spring_security.dto.UserDto;
+import com.example.spring_security.model.Role;
 import com.example.spring_security.repo.UserRepo;
-import org.apache.catalina.User;
+import com.example.spring_security.repo.UserService;
+import com.example.spring_security.model.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Collections;
+import java.util.List;
+
 
 @Service
-public class AppUserService implements  com.example.spring_security.service.UserService{
+public class AppUserService implements UserService {
+
 
     private final UserRepo userRepo;
     private final BCryptPasswordEncoder encoder;
 
-    public AppUserService(UserRepo userRepo, BCryptPasswordEncoder encoder){
+
+    public AppUserService(UserRepo userRepo, BCryptPasswordEncoder encoder) {
         this.userRepo = userRepo;
         this.encoder = encoder;
     }
 
     @Override
-    public void save(UserDto userDto) {
+    public void save(UserDto userdto) {
         User user = new User();
-        user.setUsername(userDto.getUsername());
-        user.setPassword(encoder.encode(userDto.getPassword()));
-        user.setName(userDto.getName() + " " + userDto.getSurname());
+        user.setUsername(userdto.getUsername());
+        user.setPassword(encoder.encode(userdto.getPassword()));
+        user.setName(userdto.getName() + " " + userdto.getSurname());
         user.setRoles(Collections.singleton(Role.USER));
         userRepo.save(user);
     }
-    @Override
-    public User findUSer(String username) { return userRepo.findByUsername(username); }
 
+    @Override
+    public User findUser(String username) {
+        return userRepo.findByUsername(username);
+    }
+
+    @Override
+    public List<User> findAllUsers() {
+        return userRepo.findAll();
+    }
+
+
+    @Override
+    public boolean isUsernameAvailable(String username) {
+        return userRepo.existsByUsername(username);
+    }
 }
--- src/main/java/com/example/spring_security/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.example.spring_security.service;
-
-import com.example.spring_security.model.UserDto;
-import org.apache.catalina.User;
-
-public interface UserService {
-    void save(UserDto userDto);
-    User findUSer(String username);
-}
